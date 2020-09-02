77a930d73fe51de72b8bae2f8319fa026e0de301
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-09-01 20:09:34 -0400

    mvp

--- express-router.js
+++ express-router.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const Posts = require('./data/db.js');
+// const shortid = require('shortid');
+const {
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+    findPostComments,
+    findCommentById,
+    insertComment,
+  } = require("./data/db.js");
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    const something = req.body
-    Posts.add(something)
+router.post("/", (req, res) => {    
+    Posts.insert({
+        title: req.body.title,
+        contents: req.body.contents,
+    })
     .then(post => {
-        if (!something.title || something.contents) {
+        if (!req.body.title || !req.body.contents) {
             res.status(400).json({ errorMessage: 'Please provide title and contents for the post.'})
         } else {
             res.status(201).json(post)
@@ -18,14 +31,17 @@ router.post("/", (req, res) => {
     })
 });
 router.post("/:id/comments", (req, res) => {
-    const something = req.body
-    const id = req.params.id
-    Posts.add(something)
-    .then(post => {
-        if (something.id !== id) {
-            res.status(404).json({message: 'The post with the specified ID does not exist.'})
+    Posts.insertComment({
+        text: req.body.text,
+        post_id: req.params.id,
+    })
+    .then(comment => {
+        if (!req.body.text) {
+            res.status(400).json({message: 'Please provide text for the comment.'})
+        } else if (comment) {
+            res.status(201).json(comment)
         } else {
-            res.status(201).json(post)
+            res.status(404).json({message: 'The post with the specified ID does not exist.'})
         }
     })
     .catch(error => {
@@ -34,48 +50,71 @@ router.post("/:id/comments", (req, res) => {
     })
 });
 router.get("/", (req, res) => {
-    Posts.find(req.query)
+    Posts.find()
     .then(post => {
         res.status(200).json(post)
     })
     .catch(error => {
         console.log(error);
-        res.status(500).json({message: 'there was an error'})
+        res.status(500).json({error: 'The posts information could not be retrieved.'})
     })
 });
-router.get("/:id/comments", (req, res) => {
-    Posts.add(req.body)
-    .then(post => {
-        res.status(200).json(post)
-    })
-    .catch(error => {
-        console.log(error)
-        res.status(500).json({message: 'there was an error'})
-    })
-});
-router.delete("/:id/", (req, res) => {
-    Posts.add(req.body)
-    .then(post => {
-        res.status(200).json(post)
-    })
-    .catch(error => {
-        console.log(error)
-        res.status(500).json({message: 'there was an error'})
-    })
-});
-router.put("/:id/", (req, res) => {
-    const changes = req.body
-    Posts.add(req.body)
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
     .then(post => {
         if (post) {
             res.status(200).json(post)
         } else {
-            res.status(404).json({message: 'cannot find'})
+            res.status(404).json({message: 'The post with the specified ID does not exist.'})
         }
     })
     .catch(error => {
         console.log(error)
-        res.status(500).json({message: 'there was an error'})
+        res.status(500).json({error: 'The post information could not be retrieved.'})
+    })
+});
+router.get("/:id/comments", (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: 'The post information could not be retrieved.'})
+    })
+});
+router.delete("/:id/", (req, res) => {
+    Posts.remove(req.params.id)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({message: 'The post has been deleted.'})
+        } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: 'The post could not be removed.'})
+    })
+});
+router.put("/:id/", (req, res) => {
+    Posts.update(req.params.id, req.body)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else if (!req.body.title || !req.body.contents) {
+            res.status(400).json({ errorMessage: 'Please provide title and contents for the post.'})
+        } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: 'The post information could not be motified.'})
     })
 });
 
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const server = express();
 const postRouter = require('./express-router.js');
+const port = 5679;
 
 server.use(express.json());
 server.use('/api/posts', postRouter);
 
-server.listen(5679, () => {
-    console.log('\n*** Server Running on htto://localhost:5679 ***\n');
+server.get('/', (req, res) => {
+    res.json({ message: 'Welcome to our API'})
+})
+server.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`);
 })
